d774e4b5ca800bb71247ac607e69de4826cc9948
Gigher <gastelo1301@gmail.com>
2024-11-07 16:42:13 +0300

    update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [iframeWidth, setIframeWidth] = useState("1000");
+  const [iframeHeight, setIframeHeight] = useState("600");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [inputValueWidth, setInputValueWidth] = useState("1000");
+  const [inputValueHeight, setInputValueHeight] = useState("600");
+
+  const handleApply = () => {
+    setIframeWidth(inputValueWidth);
+    setIframeHeight(inputValueHeight);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleApply();
+    }
+  };
+
+  return (
+    <div className="py-20 flex flex-col justify-between items-center min-h-screen">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start">
+        <h1 className="w-full text-center text-5xl font-normal">
+          Neurons widget test
+        </h1>
+        <div className="w-full flex gap-6 flex-wrap items-end justify-center">
+          <div>
+            <label htmlFor="width-input" className="">
+              Ширина
+            </label>
+            <Input
+              id="width-input"
+              value={inputValueWidth}
+              onChange={(e) => setInputValueWidth(e.target.value)}
+              onKeyPress={handleKeyPress}
+              title="Width"
+              placeholder="Width"
+              className="w-32"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div>
+            <label htmlFor="heitgh-input" className="">
+              Высота
+            </label>
+            <Input
+              id="height-input"
+              value={inputValueHeight}
+              onChange={(e) => setInputValueHeight(e.target.value)}
+              onKeyPress={handleKeyPress}
+              title="Height"
+              placeholder="Height"
+              className="w-32"
+            />
+          </div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button onClick={handleApply}>Применить</Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Нажмите, чтобы применить новые размеры виджета</p>
+              </TooltipContent>
+            </Tooltip>{" "}
+          </TooltipProvider>
+        </div>
+        <div className="flex justify-center items-center border-2 m-0">
+          <iframe
+            src="https://neurons.widget.dev2.webant.ru/"
+            width={iframeWidth}
+            height={iframeHeight}
+            style={{
+              transition: "all 0.3s ease-in-out",
+            }}
+          ></iframe>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
